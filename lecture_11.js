c92476a9f521a4281f8ad7916912cad0ba986bca
yadav-amit11 <ayadav.ay2002@gmail.com>
2024-12-01 18:31:33 +0530

    strings

---
+++ lecture_11.js
@@ -0,0 +1,14 @@
+// strings
+const str1="amit"
+console.log(str1);
+const str2= new String('amit')
+console.log(str2);
+const num=1.943
+console.log(num.toFixed(2));
+console.log(num.toPrecision(3));
+const num3=10000000
+console.log(num3.toLocaleString('en-IN'));
+
+// string interpolation means using  ``
+{`my name is ${varName} and age is ${age}`}
+
