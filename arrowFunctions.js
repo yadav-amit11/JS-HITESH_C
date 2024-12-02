77f66367063bb087b2b798d77545597ef2562174
yadav-amit11 <ayadav.ay2002@gmail.com>
2024-12-02 21:59:38 +0530

    CALL_STACK

---
+++ arrowFunctions.js
@@ -0,0 +1,9 @@
+// arrow functions
+const myFunc = (num1, num2) => {
+    return num1 + num2; // explicit return 
+};
+console.log(myFunc(2,2));
+
+//other way implicit return
+const myFunc2=(num3,num4) => (num3+num4)
+
---
+++ callStack_IIFE.js
@@ -0,0 +1,22 @@
+// immediately invoked function expression
+//named iife
+(function myFunc22(){
+    console.log('IIFE DEMO 1');
+    
+})(); // IIFE CALL
+
+// example 2
+((name)=>{
+    console.log(`connected to user ${name}`)
+})("amit")
+
+/*
+1.JS creates Global excution context 
+2.Next it creates memory phase
+3.Memory phase - In this phase,the variables are set to undefined 
+  until the execution phase(next phase) and the functions are set to their definitions.
+4.Next it creates the execution phase
+5.Execution phase - In this phase,the variables are initialsed to given values and 
+  when the function s are called,it creates a memory phase and execution phase for the function 
+  just like the main one.
+  */
